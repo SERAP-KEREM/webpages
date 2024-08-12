03074908dc2204ba9ba509315cca2f49742b52c6
Serap Kerem <66235306+SERAP-KEREM@users.noreply.github.com>
2024-08-12 23:02:31 +0300

    about and services

--- script.js
+++
@@ -1,9 +0,0 @@
-function openSlidebar() {
-    document.getElementById("slidebar").style.left = "0";
-    document.getElementById("main").style.marginLeft = "250px";
-}
-
-function closeSlidebar() {
-    document.getElementById("slidebar").style.left = "-250px";
-    document.getElementById("main").style.marginLeft = "0";
-}
