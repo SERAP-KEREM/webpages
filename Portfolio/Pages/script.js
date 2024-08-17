ff2a540506778de39f792556d3a5d5d8a49a0d85
Serap Kerem <66235306+SERAP-KEREM@users.noreply.github.com>
2024-08-17 23:32:17 +0300

    Portfolio page updated.

--- Portfolio/Pages/script.js
+++ Portfolio/Pages/script.js
@@ -1,20 +1,44 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Mevcut sayfanın URL'ini al
-  const currentUrl = window.location.href;
-  
-  // Tüm butonları seç
-  const navLinks = document.querySelectorAll(".aside .nav li a");
+function showProject(projectId) {
+  // Tüm projeleri gizle
+  var projects = document.querySelectorAll('.project-details');
+  projects.forEach(function(project) {
+    project.classList.remove('active');
+  });
+
+  // Seçili projeyi göster
+  var selectedProject = document.getElementById(projectId);
+  selectedProject.classList.add('active');
 
   // Tüm butonlardan 'active' sınıfını kaldır ve ilgili butona 'active' sınıfı ekle
-  navLinks.forEach(link => {
-      // Tüm butonların 'active' sınıfını temizle
-      link.classList.remove("active");
+  var navLinks = document.querySelectorAll(".aside .nav li a");
+  navLinks.forEach(function(link) {
+    link.classList.remove('active');
+    
+    // Tıklanan projenin id'si ile href değerini karşılaştır
+    if (link.getAttribute('href').includes(projectId)) {
+      link.classList.add('active');
+    }
+  });
+}
 
-      // Eğer butonun href'i mevcut URL ile eşleşiyorsa, ona 'active' sınıfı ekle
-      if (currentUrl.includes(link.href)) {
-          link.classList.add("active");
-      }
+// Sayfa yüklendiğinde ve geri/ileri tuşlarına basıldığında çalışacak işlevi tanımla
+function handleNavigation() {
+  var currentHash = window.location.hash.substring(1);
+
+  if (currentHash) {
+    // Eğer hash varsa, o projeyi göster
+    showProject(currentHash);
+  } else {
+    // Hash yoksa, varsayılan olarak ilk projeyi göster
+    showProject('gta-clone');
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  handleNavigation();
+
+  // Geri/ileri tuşlarına basıldığında sayfayı yeniden yüklemek yerine projeyi değiştir
+  window.addEventListener('popstate', function() {
+    handleNavigation();
   });
 });
-
-
