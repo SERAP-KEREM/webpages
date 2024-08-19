a6e864c59835f9abb66a10831eb3a46fc0538b3b
Serap Kerem <66235306+SERAP-KEREM@users.noreply.github.com>
2024-08-19 19:02:44 +0300

    video edit

--- Portfolio/Pages/js/script.js
+++ Portfolio/Pages/js/script.js
@@ -1,19 +1,38 @@
+// Projeyi gösteren işlev
 function showProject(projectId) {
   // Tüm projeleri gizle
   var projects = document.querySelectorAll('.project-details');
   projects.forEach(function(project) {
     project.classList.remove('active');
+    
+    // Videoyu durdur
+    var video = project.querySelector('video');
+    if (video) {
+      video.pause();
+      video.currentTime = 0;
+    }
   });
 
   // Seçili projeyi göster
   var selectedProject = document.getElementById(projectId);
-  selectedProject.classList.add('active');
+  if (selectedProject) {
+    selectedProject.classList.add('active');
+
+    // Tıklama ile video başlatmayı kontrol et
+    var video = selectedProject.querySelector('video');
+    if (video) {
+      // Video başlatma işlevini ekle
+      video.addEventListener('click', function() {
+        video.play();
+      });
+    }
+  }
 
   // Tüm butonlardan 'active' sınıfını kaldır ve ilgili butona 'active' sınıfı ekle
   var navLinks = document.querySelectorAll(".aside .nav li a");
   navLinks.forEach(function(link) {
     link.classList.remove('active');
-    
+
     // Tıklanan projenin id'si ile href değerini karşılaştır
     if (link.getAttribute('href').includes(projectId)) {
       link.classList.add('active');
@@ -35,7 +54,6 @@ function handleNavigation() {
 }
 
 document.addEventListener("DOMContentLoaded", function() {
- 
   handleNavigation();
   // Geri/ileri tuşlarına basıldığında sayfayı yeniden yüklemek yerine projeyi değiştir
   window.addEventListener('popstate', function() {
